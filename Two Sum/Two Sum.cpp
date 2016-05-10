de75260b313cd5908947f4bfce85e449f22dd03d
Bright <rosszl@163.com>
2016-05-10 09:54:21 +0800

    Update Two Sum.cpp
    更换成java

--- Two Sum/Two Sum.cpp
+++ Two Sum/Two Sum.cpp
@@ -4,35 +4,17 @@
 */
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        vector<int> res;
-        int n = numbers.size();
-        int *a = new int[n];
-        for(int i = 0;i < n;i++)    a[i] = numbers[i];
-        sort(numbers.begin(),numbers.end());
-        int lhs = 0,rhs = n-1;
-        while(rhs > lhs){
-            if(numbers[lhs] + numbers[rhs] > target)    rhs--;
-            else if(numbers[lhs] + numbers[rhs] < target)   lhs++;
-            else{
-                int id1,id2;
-                for(int i = 0;i < n;i++){
-                    if(a[i] == numbers[lhs]){
-                        id1 = i + 1;
-                        break;
-                    }
-                }
-                for(int i = n - 1;i >= 0;i--){
-                    if(a[i] == numbers[rhs]){
-                        id2 = i + 1;
-                        break;
-                    }
-                }
-                res.push_back(min(id1,id2));
-                res.push_back(max(id1,id2));
-                return res;
-            }
-        }
-    }
+	    public int[] twoSum(int[] nums, int target) {
+	        int[] num=new int[2];
+	        for(int i=0;i<nums.length-1;++i){
+	            for(int j=i+1;j<nums.length;++j){
+	               int sum=nums[i]+nums[j];
+	               if(sum==target){
+	            	 num[0]=i;
+	            	 num[1]=j;
+	               }
+	            }
+	        }
+			return num;
+	    }
 };
